c1ce2f5ef2efd669533a2a772fb03cc7aea7a568
keshav25004183-cloud <keshav.25004183@kiet.edu>
2026-02-27 10:05:12 +0530

    Add files via upload

---
+++ wordscrembling.py
@@ -0,0 +1,11 @@
+import random
+
+words = ["apple","banana","computer","python","school","keyboard","internet","program","science","library"]
+word = random.choice(words)
+scrambled = ''.join(random.sample(word, len(word)))
+print("Unscramble the word:", scrambled)
+guess = input("Your guess: ")
+if guess.lower() == word:
+    print("Correct! 🎉")
+else:
+    print("Wrong! The word was:", word)
